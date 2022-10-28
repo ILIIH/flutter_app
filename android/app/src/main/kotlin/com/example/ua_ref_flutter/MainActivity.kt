369a546a7e1f95eaa44049667aa4db2a72f8fdef
ILIIH <jointlord0@gmail.com>
2022-10-28 22:24:58 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/ua_ref_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ua_ref_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/consts.dart
@@ -0,0 +1,4 @@
+import 'dart:ui';
+
+const mainColor = Color(0xFFE06C88);
+
