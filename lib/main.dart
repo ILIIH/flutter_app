f70e3d00b638e28da09a2a72da2436aa6c5c1be8
ILIIH <jointlord0@gmail.com>
2022-10-29 00:11:01 +0100

    added text input and layout

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -41,26 +42,53 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Container(
         height: double.infinity,
         width: double.infinity,
         color: mainColor,
-        alignment: Alignment.center,
         child: Column(
-          children:  [
-            Image.asset('assets/images/hearts.png'),
-            Text("I love you Babe")
+          children:   [
+                const Padding(
+                padding: EdgeInsets.only(left:20, bottom: 0, right: 20, top:60), //apply padding to some sides only
+                  child:Text("Wishes of my love baby",
+                      style: TextStyle(fontFamily: 'Roboto',
+                      color: Colors.white,
+                      fontSize: 20)
+                      ),
+                    ),
+                Padding(
+                  padding: EdgeInsets.only(left:20, bottom: 0, right: 20, top:20), //apply padding to some sides only
+                  child:TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter type of wishes',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10)
+                    )
+                  ),
+                ),
+               ),
+              Padding(
+                padding: EdgeInsets.only(left:0, bottom: 0, right: 0, top:38), //apply padding to some sides only
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                      borderRadius: BorderRadius.all(Radius.circular(20))
+                  ),
+                  height: MediaQuery. of(context). size. height - 200,
+                  width: double.infinity,
+                          )
+              ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
+
+
